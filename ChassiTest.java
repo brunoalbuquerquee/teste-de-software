441eea1e4de7182a2aaf910b5d1d8c793bf1f7e4
Bruno Albuquerque <trabalho.brunoalbuquerque@gmail.com>
2022-03-24 08:35:16 -0300

    Update ChassiTest.java

--- ChassiTest.java
+++ ChassiTest.java
@@ -1,2 +1,30 @@
 //Precisa de 2 Asserts
 //O Primeiro pode ser verificar se tem 17 numeros o chassi("9BP17164GA0000001")
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ChassiTest{
+
+    
+    public ChassiTest() {
+    }
+    
+    Chassi objChassi = new Chassi();
+
+    @Test
+    public void testChassiInvalido() {
+        System.out.println("Tamanho chassi invalido");
+        assertEquals("Nota Inválida", objChassi.validaChassi(-1));
+    }
+    
+    @Test
+    public void testChassiValido() {
+        System.out.println("Tamanho do chassi válido");
+        assertEquals("Chassi válido", objNota.validaChassi(0));
+    }
+    
+    
+    
+}
+
