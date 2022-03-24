84a6a8c9d0722461e0b4e164015d6eb549c033df
Bruno Albuquerque <trabalho.brunoalbuquerque@gmail.com>
2022-03-24 08:18:53 -0300

    Create Chassi.java
    Programa que verifica chassi.

---
+++ Chassi.java
@@ -0,0 +1,43 @@
+public class Chassi {
+    
+    private String numero;
+
+    public Chassi(String numero) {
+        this.numero = numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    public void maiusculo(){
+        this.numero = numero.toUpperCase();
+    }
+    
+    public String incompleto(){
+        if(numero.length() == 17){
+            return "Completo";
+        } else if (numero.length() < 17){
+            return "Inválido tamanho menor";
+        } else {
+            return "Inválido tamanho maior";
+        }
+    }
+    
+    
+    public int getAno(){
+        char ano = numero.charAt(9);
+        
+        switch(ano){
+            case 'A': 
+                return 10;
+            case 'B':
+                return 11;
+            case 'C':
+                return 12;
+            default:
+                return -1;
+        }
+    }
+        
+    }
